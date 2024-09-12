70e39ea142f7248e715113343a8ac55d6e70b3ab
Kabir <kabirshuvo19@gmail.com>
2024-09-13 04:50:48 +0600

    home page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>TJ&PALS</h1>
+      <h2>&quot;This is app&quot; is Under Development</h2>
     </div>
   );
 }
