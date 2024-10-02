d362a91138a5e8a3d4b3b4bbc067328feb7a3a69
Kabir <kabirshuvo19@gmail.com>
2024-10-02 10:35:54 +0600

    MenuPage Updated: Making pixel Perfect

--- src/app/[locale]/(site)/page.tsx
+++ src/app/[locale]/(site)/page.tsx
@@ -11,7 +11,7 @@ const Home: React.FC = () => {
   const categories = useCategories();
 
   return (
-    <div className="container mx-auto px-2 grid grid-cols-2 sm:grid-cols-2 md:grid-cols-6 gap-2 md:gap-4 flex-1 flex-grow h-full">
+    <div className="container mx-auto  grid grid-cols-2 sm:grid-cols-2 md:grid-cols-6 gap-2 md:gap-4 flex-1 flex-grow h-full">
       {categories.map((category) => (
         <Link
           key={category.id}
@@ -20,16 +20,16 @@ const Home: React.FC = () => {
         >
           <div className="h-full flex flex-col gap-2">
             <div className="flex-1">
-              <h3 className="lg:text-xl xl:text-2xl text-center uppercase">
+              <h3 className="lg:text-xl xl:text-2xl text-center uppercase text-pretty ">
                 {category.name}
               </h3>
             </div>
-            <div className="flex justify-center items-center bg-white rounded-lg shadow-lg hover:shadow-xl transform transition duration-200 h-full cursor-pointer border-4 border-transparent hover:border-4 hover:border-red-500">
+            <div className="flex justify-center items-center bg-white  hover:shadow-xl transform transition duration-200 h-full cursor-pointer  hover:border-4 hover:border-red-500">
               <Image
                 src={category.image}
                 alt={category.name}
-                width={400}
-                height={400}
+                width={380}
+                height={380}
                 priority
                 layout="responsive"
                 className="w-full h-full object-cover flex-grow flex-1 flex"
